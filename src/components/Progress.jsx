5ac3275c17876b35480c6b01981df889a87e65eb
Huertz <104065687+Huertz@users.noreply.github.com>
2024-10-07 23:28:01 -0500

    added progress bar

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,6 +1,10 @@
 function Progress({ index, numQuestions, points, maxPossiblePoints }) {
   return (
     <header className='progress'>
+      <progress
+        max={numQuestions}
+        value={index}
+      />
       <p>
         Question <strong>{index + 1}</strong> / {numQuestions}
       </p>
